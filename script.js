53cf892b2d15c3edb2466c35ad7c5d610c0efd83
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-04-21 12:02:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const navBar = document.querySelector('.bar');
 const ul = document.querySelector('.ul');
+const header = document.querySelector('.header')
+const ulHeight = ul.getBoundingClientRect();
+const fixedHeader = document.querySelector('.header');
+
 
 navBar.addEventListener('click', function(){
-    
+    // console.log(ulHeight);
 navBar.classList.toggle("active");
 ul.classList.toggle("active");
 
+})
+
+ul.addEventListener('click', function(){
+    ul.classList.toggle("active");
+    navBar.classList.toggle("active");
+})
+
+
+window.addEventListener('scroll', function(){
+    // console.log(window.pageYOffset);
+    const scrollHeight = window.pageYOffset;
+    const navHeight = fixedHeader.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        fixedHeader.classList.add('fixed-header')   
+    }
+    else{
+        fixedHeader.classList.remove('fixed-header') 
+    }
 })
